20951534dd113cd8473c2b654daf2df0be0a02b3
13priyankameena <13priyankameena13@gmail.com>
2025-10-18 11:43:33 +0530

    ...

--- controller/otpController.js
+++ controller/otpController.js
@@ -19,9 +19,9 @@ const transporter = nodemailer.createTransport({
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
-    // tls: {
-    //     rejectUnauthorized: false, // allow self-signed certs
-    // },
+    tls: {
+        rejectUnauthorized: false, // allow self-signed certs
+    },
 })
 
 //send OTP
